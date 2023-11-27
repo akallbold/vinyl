8f465754c64cde1298989e12702453ca24a8af46
Anna <a.kallenbornbolden@gmail.com>
2023-11-27 15:07:17 +0100

    Initial Commit

---
+++ vinyl/vinylApp.swift
@@ -0,0 +1,17 @@
+//
+//  vinylApp.swift
+//  vinyl
+//
+//  Created by Anna on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct vinylApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
